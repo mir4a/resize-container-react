bc3f1fcd29a2e44197e1acb23dcec7dd18dff12a
Miroslav Martynov <marty.mircha@gmail.com>
2022-10-31 14:32:43 +0100

    add sample test, rollup tune

--- rollup.config.js
+++ rollup.config.js
@@ -32,7 +32,7 @@ export default [
   {
     input: "dist/esm/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts()],
+    plugins: [dts.default()],
     external: [/\.css$/],
   },
 ];
