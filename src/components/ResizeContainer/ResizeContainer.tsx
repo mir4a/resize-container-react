789188f3116859d6741af6f77e39414493ea047c
Miroslav Martynov <marty.mircha@gmail.com>
2022-10-31 22:19:22 +0100

    add width resize

--- src/components/ResizeContainer/ResizeContainer.tsx
+++ src/components/ResizeContainer/ResizeContainer.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import ResizeHeightHandle from "../ResizeHeightHandle";
+import ResizeWidthHandle from "../ResizeWidthHandle";
 
 import "./ResizeContainer.css";
 
@@ -11,39 +12,64 @@ export interface ResizeContainerProps {
   onResizeEnd?: () => void;
 }
 
+export type DragParams = {
+  deltaX?: number;
+  deltaY?: number;
+};
+
 const ResizeContainer: React.FC<ResizeContainerProps> = ({ children }) => {
   const ref = React.useRef<HTMLDivElement>(null);
   const [height, setHeight] = React.useState<number | undefined>(() =>
     ref.current ? ref.current.offsetHeight : undefined
   );
+  const [width, setWidth] = React.useState<number | undefined>(() =>
+    ref.current ? ref.current.offsetWidth : undefined
+  );
   const [prevHeight, setPrevHeight] = React.useState<number | undefined>(() =>
     ref.current ? ref.current.offsetHeight : undefined
+  );
+  const [prevWidth, setPrevWidth] = React.useState<number | undefined>(() =>
+    ref.current ? ref.current.offsetWidth : undefined
   );
 
   React.useLayoutEffect(() => {
     if (ref.current) {
       setPrevHeight(ref.current.offsetHeight);
+      setPrevWidth(ref.current.offsetWidth);
     }
   }, []);
 
-  const onDrag = ({ deltaY }: { deltaY: number }) => {
-    if (ref.current && prevHeight) {
-      console.log("onDrag", deltaY);
-      deltaY && setHeight(prevHeight + deltaY);
+  const onDrag = ({ deltaX, deltaY }: DragParams) => {
+    if (ref.current) {
+      deltaX && prevWidth && setWidth(prevWidth + deltaX);
+      deltaY && prevHeight && setHeight(prevHeight + deltaY);
     }
   };
 
+  const onResizeHeight = ({ deltaY }: DragParams) => {
+    onDrag({ deltaY });
+  };
+
+  const onResizeWidth = ({ deltaX }: DragParams) => {
+    onDrag({ deltaX });
+  };
+
+  const onResizeBoth = ({ deltaX, deltaY }: DragParams) => {
+    onDrag({ deltaX, deltaY });
+  };
+
   const onDragEnd = () => {
-    console.log("onDragEnd");
     if (ref.current) {
       setPrevHeight(ref.current.offsetHeight);
+      setPrevWidth(ref.current.offsetWidth);
     }
   };
 
   return (
-    <div className="resize-container" ref={ref} style={{ height }}>
+    <div className="resize-container" ref={ref} style={{ height, width }}>
       {children}
-      <ResizeHeightHandle onDrag={onDrag} onDragEnd={onDragEnd} />
+      <ResizeHeightHandle onDrag={onResizeHeight} onDragEnd={onDragEnd} />
+      <ResizeWidthHandle onDrag={onResizeWidth} onDragEnd={onDragEnd} />
     </div>
   );
 };
---
+++ src/components/ResizeWidthHandle/ResizeWidthHandle.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+import { ReactComponent as ArrowWE } from "../icons/ArrowWE.svg";
+import "./ResizeWidthHandle.css";
+
+export interface ResizeWidthHandleProps {
+  onDrag: ({ deltaX }: { deltaX: number }) => void;
+  onDragEnd: () => void;
+  children?: React.ReactNode;
+}
+
+const ResizeWidthHandle: React.FC<ResizeWidthHandleProps> = ({
+  children,
+  onDrag,
+  onDragEnd,
+}) => {
+  const ref = React.useRef<HTMLDivElement>(null);
+  React.useLayoutEffect(() => {
+    if (ref.current) {
+      ref.current.onmousedown = (e) => {
+        e.preventDefault();
+        const currentPositionX = e.pageX;
+
+        function onMouseMove(mouseMoveEvent: MouseEvent) {
+          const deltaX = mouseMoveEvent.pageX - currentPositionX;
+          onDrag({ deltaX });
+        }
+
+        document.addEventListener("mousemove", onMouseMove);
+
+        document.onmouseup = () => {
+          document.removeEventListener("mousemove", onMouseMove);
+          document.onmouseup = null;
+          onDragEnd();
+        };
+      };
+    }
+  }, [onDrag, onDragEnd]);
+
+  return (
+    <div className="resize-handle resize-width-handle" ref={ref}>
+      {children ? children : <ArrowWE />}
+    </div>
+  );
+};
+
+export default ResizeWidthHandle;
